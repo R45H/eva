26d783c871c156f3c8f3db6bc2bcc0514795ae22
alexandr <al.rout@yandex.ru>
2017-04-21 17:35:50 +0300

    add page item

--- app/src/blocks/slick/slick.js
+++ app/src/blocks/slick/slick.js
@@ -1,1 +1,16 @@
-$('.slick').slick();
+$('.slider-for').slick({
+	slidesToShow: 1,
+	arrows: true,
+	fade: true,
+	asNavFor: '.slick-element'
+
+});
+
+$('.slick-element').slick({
+	slidesToShow: 2,
+	asNavFor: '.slider-for',
+	centerMode: true,
+	focusOnSelect: true,
+	arrows: false,
+	variableWidth: true
+});
--- app/src/script.js
+++ app/src/script.js
@@ -2,4 +2,7 @@ $(function() {
 	@@include('blocks/s-menu/s-menu.js') // Боковое меню в шапке
 	@@include('blocks/aside/aside.js') // Боковое меню
 	@@include('blocks/slick/slick.js') // Слайдер карточки объекта
+	@@include('blocks/slick_tabs/tabs.js') // Табы цвета
+	@@include('blocks/tab-size/tab-size.js') // Табы размера
 });
+
